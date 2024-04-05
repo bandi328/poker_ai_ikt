f9533ebed933c7eaab7b92d8a911bbe5773b418e
bandi328 <moldovan.andras@students.jedlik.eu>
2024-04-05 21:02:11 +0200

    játékmenet, AI feladatok leírása (kezdetleges)

--- Poker_AI/Poker_AI/AI.cs
+++ Poker_AI/Poker_AI/AI.cs
@@ -8,5 +8,28 @@ namespace Poker_AI
 {
     public class AI
     {
+        //kombinációk keresése, sorbaállítása, legjobb kiválasztása
+            //1-10 pontozás, súlyozás (kivéve Royal Flush, ez esetén akár ALL IN) lap értékéből
+        //       szín,szám (pontos egyezést néz)
+
+            //10 5,   5 (AKQJ10)
+            //9  5,   5 (szám egymást követi)
+            //8  0,   4
+            //7  0,   3+2
+            //6  5,   0
+            //5  0,   5 (szám egymást követi)
+            //4  0,   3
+            //3  0,   2+2
+            //2  0,   2
+            //1  0,   0 (magas lap)
+
+        //esély számolása kiválasztott kombinációból
+            
+        //esély alapján tét rakása/dobás/emelés/skippelés
+
+        public int Hands()
+        {
+
+        }
     }
 }
--- Poker_AI/Poker_AI/Main.cs
+++ Poker_AI/Poker_AI/Main.cs
@@ -1,3 +1,24 @@
 ﻿using Poker_AI;
 
 Cards cards = new Cards();
+
+//Játékmenet:
+//10$/20$ játék
+//200$ tőke
+//alap tétek megadása (kötelező, hogy mindenképp kelljen játszani)
+    //először a JÁTÉKOS a "kis vak", 5$ beszálló
+    //AI kártyái alapján tartja/emel
+    //ha AI emel, JÁTÉKOS dönthet a megadás/eldobás/emelés közül (max 3 emelés lehetséges, utána megadás/eldobás)
+    //ha AI nem emel =>
+//DEALER oszt 3 lapot (1-et éget előtte)
+
+    //kör
+        //"kis vak" tétet rak/skippel/eldob
+        //"nagy vak" tart/emel/eldob
+
+        //ha bármelyik is az AI: lapok alapján tétet rak/tart/skippel/eldob
+
+    //3 emelés vagy végleges megadés után új lap 1 égetéssel, még egy kör jön
+
+//3 kör után lapok mutatása, pénz jóváírása, új kör.
+//amíg valaki nem nyer, vagy amíg a játékos úgy nem dönt megy a játék
